11294177514b4e61f2b45fe728cf17c6fbca24b9
Andrew Herring <andrewoherring@gmail.com>
2019-01-09 13:52:15 -0600

    first push

---
+++ cars.py
@@ -0,0 +1,9 @@
+showroom = {"honda", "toyota", "mazda", "cadillac"}
+print ("length", len(showroom))
+showroom.add("mazda")
+print(showroom)
+showroom.update({"rocket", "boat"})
+print(showroom)
+showroom.discard("boat")
+print(showroom)
+
---
+++ stocks.py
@@ -0,0 +1,10 @@
+stock_dict = { 'Blizz': 'Blizzard',
+ 'EA':'Electronic Arts', 'FP':"Face Punch Studios" }
+
+purchases = [ ( 'Blizz', 100, '10-sep-2001', 48 ),
+ ( 'EA', 100, '1-apr-1999', 24 ),
+ ( 'Blizz', 200, '1-jul-1998', 56 ) ]
+
+for price in purchases:
+  print("I purchased", {1})
+
